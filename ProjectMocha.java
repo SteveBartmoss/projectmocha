dc70d60c5da3b8be99f63febebb98c63b2772ab9
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-07 21:09:54 -0600

    se creo el primer hola mundo en la libreria fx

---
+++ ProjectMocha.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ProjectMocha extends Application{
+
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("mochaEditor");
+
+        StackPane root = new StackPane();
+
+        root.getChildren().add(new Label("hola"));
+
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
