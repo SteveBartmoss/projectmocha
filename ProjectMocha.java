fe0165eb07f6c7eb023ab55a0407222898ca6f20
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-08 08:58:35 -0600

    Update ProjectMocha.java
    Se agregaron dos contenedores, para simular la ventana principal y el explorador lateral de un editor de texto

--- ProjectMocha.java
+++ ProjectMocha.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx,scene.layout.VBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -9,9 +11,18 @@ public class ProjectMocha extends Application{
     public void start(Stage primaryStage){
         primaryStage.setTitle("mochaEditor");
 
-        StackPane root = new StackPane();
+        BorderPane root = new BorderPane();
 
-        root.getChildren().add(new Label("hola"));
+        VBox leftPane = new VBox();
+        Label explorerLabel = new Label("Explorador");
+        leftPane.getChildren().add(explorerLabel);
+
+        VBox centerPane = new VBox();
+        Label mainWindowLabel = new Label("Ventana principal");
+        centerPane.getChildren().add(mainWindowLabel);
+
+        root.setLeft(leftPane);
+        root.setCenter(centerPane);
 
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
