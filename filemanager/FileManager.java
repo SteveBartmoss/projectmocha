14550a7c3756409b9bf8040df356b3f002af940e
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2024-08-13 09:09:15 -0600

    Update FileManager.java
    Se agrego una funcion para recibir un archivo y luego leer su contenido para que se regrese el contenido como un string

--- filemanager/FileManager.java
+++ filemanager/FileManager.java
@@ -1,7 +1,9 @@
 package filemanager;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -14,5 +16,17 @@ public class FileManager {
       System.out.println("Error al guardar archivo: " + ex.getMessage());
     }
   }
-  
+
+  public string abrirArchivo(File archivo){
+    try(BufferedReader br = mew BufferedReader(new FileReader(archivo))){
+      StringBuilder sb = new StringBuilder();
+      String line;
+      while((line = br.readLine())!=null){
+        sb.append(line).append("\n");
+      }
+      return sb.toString();
+    }catch(IOException ex){
+      System.out.println("Error al leer archivo: " + ex.getMessage();
+    }
+  }
 }
