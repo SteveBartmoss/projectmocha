5fc548a5475bacea53d3a28a6b50240ead5ed90c
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-17 14:17:33 -0600

    se limpio el codigo

--- ProjectMocha.java
+++ ProjectMocha.java
@@ -133,28 +133,6 @@ public class ProjectMocha extends Application{
 
             About windowAbout = new About();
             windowAbout.getAboutInfo();
-            /*Stage aboutStage = new Stage();
-            aboutStage.setTitle("About MochaEditor");
-            aboutStage.initModality(Modality.APPLICATION_MODAL);
-
-            VBox aboutContent = new VBox();
-            aboutContent.setPadding(new Insets(10));
-            aboutContent.setSpacing(10);
-
-            Label header = new Label("MochaEditor");
-            header.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
-            Label info = new Label("MochaEditor es un editor de texto desarrollado en JavaFX. \nVersión: 1.0\nDesarrollado por: Tu Nombre");
-            
-
-            Button closeButton = new Button("Cerrar");
-            closeButton.setOnAction(e -> aboutStage.close());
-
-            aboutContent.getChildren().addAll(header, info, closeButton);
-
-            Scene aboutScene = new Scene(aboutContent, 300,150);
-            aboutScene.getStylesheets().add(getClass().getResource("themes/darkTheme.css").toExternalForm());
-            aboutStage.setScene(aboutScene);
-            aboutStage.showAndWait();*/
         });
 
         HBox topPane = new HBox();
