703d235d79fff298fc74bede5bde1ac914843d69
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-17 15:29:35 -0600

    se agrego un metodo para escuchar el cambio del contenido del archivo y asi modificar el texto de la tab

--- components/FileWindow.java
+++ components/FileWindow.java
@@ -2,6 +2,8 @@ package components;
 
 import java.io.File;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Tab;
 
@@ -21,6 +23,19 @@ public class FileWindow{
 
         textArea.setText(fileManager.abrirArchivo(archivo));
         initialContentLength = fileManager.abrirArchivo(archivo).length();
+
+        textArea.textProperty().addListener(new ChangeListener<String>(){
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue){
+                if(archivo != null){
+                    if(newValue.length() != initialContentLength){
+                        tabFile.setText(archivo.getName() + " (Modificado)");
+                    }
+                    else{
+                        tabFile.setText(archivo.getName());
+                    }
+                }
+            }
+        });
 
         return tabFile;
     }
