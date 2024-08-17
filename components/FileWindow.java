6e60434b71febd13382a56ac94d8f42bcf324f88
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-17 16:05:04 -0600

    se agrego contenido para que algunas variables sean globales para la clase fileWindow, se agrego una funcion para guardar el archivo

--- components/FileWindow.java
+++ components/FileWindow.java
@@ -4,29 +4,34 @@ import java.io.File;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.stage.FileChooser;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Tab;
+import javafx.stage.Stage;
 
 import filemanager.FileManager;
 
 public class FileWindow{
 
+    File localFile = null;
     int initialContentLength = 0;
     FileManager fileManager = new FileManager();
-
+    Tab tabFile = new Tab("");
+    TextArea textArea = new TextArea();
 
     public Tab setNewFileWindo(File archivo){
 
-        Tab tabFile = new Tab(archivo.getName());
-        TextArea textArea = new TextArea();
+        localFile = archivo;
+
+        tabFile.setText(archivo.getName());
         tabFile.setContent(textArea);
 
-        textArea.setText(fileManager.abrirArchivo(archivo));
-        initialContentLength = fileManager.abrirArchivo(archivo).length();
+        textArea.setText(fileManager.abrirArchivo(localFile));
+        initialContentLength = fileManager.abrirArchivo(localFile).length();
 
         textArea.textProperty().addListener(new ChangeListener<String>(){
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue){
-                if(archivo != null){
+                if(localFile != null){
                     if(newValue.length() != initialContentLength){
                         tabFile.setText(archivo.getName() + " (Modificado)");
                     }
@@ -38,5 +43,22 @@ public class FileWindow{
         });
 
         return tabFile;
+    }
+
+    public void saveWindowChanges(){
+        
+        if(localFile != null){
+            fileManager.guardarArchivo(localFile, textArea.getText());
+            initialContentLength = textArea.getText().length();
+        }
+        else{
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Guardar Archivo");
+            Stage tabStage = (Stage) tabFile.getTabPane().getScene().getWindow();
+            localFile = fileChooser.showSaveDialog(tabStage);
+            fileManager.guardarArchivo(localFile, textArea.getText());
+            initialContentLength = textArea.getText().length();
+        }
+        
     }
 }
