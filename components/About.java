c0d285587e6a4538a9a4f09df9c8239755e17b20
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-17 14:11:44 -0600

    se agrearon importaciones necesarias y el package

--- components/About.java
+++ components/About.java
@@ -1,4 +1,13 @@
+package components;
 
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class About {
 
