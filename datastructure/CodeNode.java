a633e3d7e91e25f2026c065fc4ecc75eb0629a56
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-19 21:43:46 -0600

    se agrego un tipo de dato para la representacion de un nodo en una lista de datos ligados que se usara para el codigo de un archivo

---
+++ datastructure/CodeNode.java
@@ -0,0 +1,15 @@
+
+
+public class CodeNode{
+    String line;
+    CodeNode next;
+    CodeNode prev;
+
+    public CodeNode(Strin line){
+        this.line = line;
+        this.next null;
+        this.prev = null;
+    }
+
+    
+}
