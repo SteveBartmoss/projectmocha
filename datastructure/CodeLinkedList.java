973cfe914797d24b39bf8a4cab582551391a2ad1
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-08-21 21:35:36 -0600

    se agrego una funcion para insertar un modo en medio de la lista

--- datastructure/CodeLinkedList.java
+++ datastructure/CodeLinkedList.java
@@ -23,6 +23,21 @@ public class CodeLinkedList {
         }
     }
 
+    public void insertAfter(CodeNode node, String line){
+        lineCount++;
+        CodeNode newNode = new CodeNode(line,lineCount);
+        if(node == tail){
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }else{
+            newNode.next = node.next;
+            node.next.prev = newNode;
+            node.next = newNode;
+            newNode.prev = node;
+        }
+    }
+
     public int getLineCount() {
         return lineCount;
     }
